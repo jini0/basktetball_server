e3c9794fe252613cce3a47734b7cef83c0772d4c
jini <cathkid00@gmail.com>
2022-08-23 14:58:17 +0900

    서버

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-//npm init
-//npm install express
-//npm install cors
+//1. npm init   //npm 초기화
+//2. npm install express
+//3. npm install cors
 const express = require("express");         //express로 만들거니까!
 const cors = require("cors");
 const app = express();
+const port =  process.env.PORT || 8001;
+
+//데이터베이스 연결
+const connection = mysql.createConnection({
+    host: parseData.host,
+    user:parseData.user,
+    password:parseData.password,
+    port:parseData.port,
+    database: parseData.database
+})
+
+// 서버실행
+app.listen(port, () => {
+    console.log('서버가 돌아가고 있습니다.');
+})
