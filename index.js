c2f09a04c02460d619cc4fd59c38048cefd1d0dd
jini <cathkid00@gmail.com>
2022-08-24 17:44:40 +0900

    서버 app.use 연결

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 //1. npm init   //npm 초기화
 //2. npm install express
 //3. npm install cors
-const express = require("express");         //express로 만들거니까!
+const express = require("express");     //express로 만들거니까!
 const cors = require("cors");
 const app = express();
 const port =  process.env.PORT || 8001;
+
+app.use(express.json());     //json형식의 데이터를 처리할수 있도록설정
+app.use(cors());    //브라우저의 다양한 사용을 위해 설정
 
 //데이터베이스 연결
 const connection = mysql.createConnection({
